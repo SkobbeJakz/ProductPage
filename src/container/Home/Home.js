0090005c81cfec303693275eb2dbf7ac196d6bd0
Ebenezer_08 <ebenezercoetzer@gmail.com>
2022-01-31 16:26:39 +0200

    New Commit

--- src/container/Home/Home.js
+++ src/container/Home/Home.js
@@ -27,9 +27,9 @@ class Home extends Component {
     let cost = selectProduct.cost;
 
     // qty controll
-    if (operator == "+") {
+    if (operator === "+") {
       qty = qty + 1;
-    } else if (operator == "-" && qty !== 0) {
+    } else if (operator === "-" && qty !== 0) {
       qty = qty - 1;
     } else if (operator >= 0) {
       qty = operator;
@@ -56,7 +56,7 @@ class Home extends Component {
   totalCartCost = (cart) => {
     // calculate total cost
     let cartTotalCost = 0;
-    Object.keys(this.state.product.options).map((productItem) => {
+    Object.keys(this.state.product.options).forEach((productItem) => {
       cartTotalCost =
         +parseFloat(cartTotalCost) + +parseFloat(cart[productItem].cost);
     });
@@ -74,6 +74,7 @@ class Home extends Component {
     this.setState({ refreshNum: randomNumber });
   };
 
+  // fetch and Parse data from API
   componentDidMount() {
     fetch("https://fe-assignment.vaimo.net/")
       .then((result) => result.json())
@@ -87,13 +88,13 @@ class Home extends Component {
     // default cart
     let cart = JSON.parse(localStorage.getItem("cart"));
     let cartProductObj = {};
-    if (!cart) {
-      Object.keys(this.state.product.options).map((productItem) => {
+    if (!cart ) {
+      Object.keys(this.state.product.options).forEach((productItem)  => {
         cartProductObj[productItem] = {
           qty: 0,
           sum: 0,
           cost: 0,
-        };
+        } 
       });
       cartProductObj.totalCost = 0;
       localStorage.setItem("cart", JSON.stringify(cartProductObj));
